572788973b00325fe152610e0e08970cc26ddada
Geraldo <gsneto652@gmail.com>
2021-03-02 00:58:06 -0300

    feat: resolvendo os servicos

---
+++ BlueModas.Api/src/BlueModas.WebApi/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,22 @@
+﻿using BlueModas.Data.Repositories;
+using BlueModas.Domain.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueModas.WebApi.Configurations
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            // repositories
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+
+
+            return services;
+        }
+    }
+}
