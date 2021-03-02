3b53b505edb90593075fde06ffc6e8cc54abd420
Geraldo <gsneto652@gmail.com>
2021-03-02 00:50:58 -0300

    feat: adicionando entidade base

---
+++ BlueModas.Api/src/BlueModas.Domain/Entity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueModas.Domain
+{
+    public abstract class Entity : IEquatable<Entity>
+    {
+        public Guid Id { get; private set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public bool Equals(Entity other)
+        {
+            return Id.Equals(other.Id);
+        }
+    }
+}
