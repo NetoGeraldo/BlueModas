f6c67bb4bacf893fc2029418551c4cde4cec4a95
Geraldo <gsneto652@gmail.com>
2021-03-02 00:58:42 -0300

    fix: adicionando mainController

---
+++ BlueModas.Api/src/BlueModas.WebApi/Controllers/MainController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueModas.WebApi.Controllers
+{
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+    }
+}
