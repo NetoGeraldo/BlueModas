d0807be832dcb5a4a45adb9e7e153619eb8ef154
Geraldo <gsneto652@gmail.com>
2021-03-02 00:58:21 -0300

    feat: produtos controller

---
+++ BlueModas.Api/src/BlueModas.WebApi/Controllers/ProdutosController.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using BlueModas.Domain;
+using BlueModas.Domain.Repositories;
+using BlueModas.WebApi.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueModas.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : MainController
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IMapper _mapper;
+
+        public ProdutosController(IProdutoRepository produtoRepository, 
+                                  IMapper mapper)
+        {
+            _produtoRepository = produtoRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObterProdutosAsync()
+        {
+            var produtos = await _produtoRepository.ObterTodosAsync();
+
+            var produtosViewModel = _mapper.Map<List<Produto>, List<ProdutoViewModel>>(produtos);
+
+            return Ok(produtosViewModel);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> ObterProdutoAsync(Guid id)
+        {
+            var produto = await _produtoRepository.ObterPorIdAsync(id);
+
+            var produtoViewModel = _mapper.Map<Produto, ProdutoViewModel>(produto);
+
+            return Ok(produtoViewModel);
+        }
+
+    }
+}
---
+++ BlueModas.Api/src/BlueModas.WebApi/ViewModels/ProdutoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueModas.WebApi.ViewModels
+{
+    public class ProdutoViewModel
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public byte[] Imagem { get; set; }
+    }
+}
