905b94f89bc71732752635161eee471184497db1
Geraldo <gsneto652@gmail.com>
2021-03-08 23:24:21 -0300

    feat: adicionando rotas

--- BlueModasApp/src/app/app.module.ts
+++ BlueModasApp/src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 
-import { AppRoutingModule } from './app-routing.module';
+import { registerLocaleData } from '@angular/common'
+import localePt from '@angular/common/locales/pt'
+registerLocaleData(localePt);
+
 import { AppComponent } from './app.component';
+import { MenuComponent } from './navigation/menu/menu.component';
+import { FooterComponent } from './navigation/footer/footer.component';
+import { HomeComponent } from './navigation/home/home.component';
+import { rootRouterConfig } from './app.routes';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { IconeCarrinhoComponent } from './icone-carrinho/icone-carrinho.component';
+import { ProdutosService } from './produtos/produtos.service';
+import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    CarrinhoComponent,
+    IconeCarrinhoComponent,
+    ListaProdutoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    [RouterModule.forRoot(rootRouterConfig, { useHash: false})]
   ],
-  providers: [],
+  providers: [
+    ProdutosService,
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ BlueModasApp/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from "@angular/router";
+import { CarrinhoComponent } from "./carrinho/carrinho.component";
+import { HomeComponent } from "./navigation/home/home.component";
+import { ListaProdutoComponent } from "./produtos/lista-produto/lista-produto.component";
+
+export const rootRouterConfig: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'produtos', component: ListaProdutoComponent},
+    { path: 'carrinho', component: CarrinhoComponent },
+]
