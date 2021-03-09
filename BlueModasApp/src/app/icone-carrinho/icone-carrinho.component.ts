4468b2383d25b52bcb44aa5453e0b34da3ba99f1
Geraldo <gsneto652@gmail.com>
2021-03-08 23:24:07 -0300

    feat: adicionando icone do carrinho de compras

---
+++ BlueModasApp/src/app/icone-carrinho/icone-carrinho.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icone-carrinho',
+  templateUrl: './icone-carrinho.component.html',
+  styleUrls: ['./icone-carrinho.component.css']
+})
+export class IconeCarrinhoComponent {
+
+  constructor() { }
+}
